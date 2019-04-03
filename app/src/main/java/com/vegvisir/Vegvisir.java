fadfbc868938f36b2887f2c35a2708d15d7ac7b7
JASONews <wxwjason@gmail.com>
2019-04-02 21:24:37 -0400

    add vegvisir gossip as a dependency

--- app/src/main/java/com/vegvisir/Vegvisir.java
+++ app/src/main/java/com/vegvisir/Vegvisir.java
@@ -1,10 +1,13 @@
 package com.vegvisir;
 
+import com.vegvisir.gossip.*;
 /**
  * Vegvisir reconciliation protocol version 0.1
  * We will use this class as a start point for vegivisir com.vegvisir.core.blockdag.
  */
 public class Vegvisir implements Runnable {
+
+    private Gossip gossip;
 
     public static void main(String argv[]) {
 
@@ -12,7 +15,6 @@ public class Vegvisir implements Runnable {
 
     @Override
     public void run() {
-
 
     }
 
