8ce1e609978ca5c2a16050e0b5d67e70bd8b3099
JASONews <wxwjason@gmail.com>
2019-04-05 16:32:32 -0400

    fix hash to the same value bug

--- vegvisirCore/src/main/java/com/vegvisir/core/blockdag/BlockUtil.java
+++ vegvisirCore/src/main/java/com/vegvisir/core/blockdag/BlockUtil.java
@@ -1,12 +1,13 @@
 package com.vegvisir.core.blockdag;
 
+import com.google.protobuf.ByteString;
 import com.isaacsheff.charlotte.proto.Reference;
 import com.isaacsheff.charlotte.proto.Block;
 
 public class BlockUtil {
 
     public static Reference byRef(Block block) {
-        return Reference.newBuilder().setHash(com.isaacsheff.charlotte.proto.Hash.newBuilder().setSha3(block.getStrBytes()).build()).build();
+        return Reference.newBuilder().setHash(com.isaacsheff.charlotte.proto.Hash.newBuilder().setSha3(ByteString.copyFrom(block.toByteArray())).build()).build();
     }
 
 }
