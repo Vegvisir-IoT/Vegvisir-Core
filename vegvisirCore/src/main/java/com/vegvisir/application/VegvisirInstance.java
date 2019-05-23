38bd993ca0b1a102180a87f40ba2f646f8ac1afe
JASONews <wxwjason@gmail.com>
2019-05-23 17:03:42 -0400

    update add transction interface

--- vegvisirCore/src/main/java/com/vegvisir/application/VegvisirInstance.java
+++ vegvisirCore/src/main/java/com/vegvisir/application/VegvisirInstance.java
@@ -2,6 +2,8 @@ package com.vegvisir.application;
 
 import com.vegvisir.core.datatype.proto.Block;
 
+import java.util.List;
+
 public interface VegvisirInstance {
 
 
@@ -15,15 +17,23 @@ public interface VegvisirInstance {
      * @param delegator a delegator instance.
      * @return true if the @delegator is successfully registered.
      */
-    public boolean registerApplicationDelegator(VegvisirApplicationContext context, VegvisirApplicationDelegator delegator);
+    public boolean registerApplicationDelegator(VegvisirApplicationContext context,
+                                                VegvisirApplicationDelegator delegator);
 
 
     /**
      * Add a new transaction to the DAG. If the transaction is valid, then it will be added to the
-     * block, either current one or next one depends on the transaction queue size.
-     * @param context  a context object of the application.
-     * @param tx a transaction to be added to the DAG.
+     * block, either current one or next one depends on the transaction queue size. If the transaction
+     * is valid, then this transaction will be pass to applyTransaction immediately to let application
+     * update its states.
+     * @param context a context object of the application.
+     * @param topic a pub/sub topic that unique identify who are interested in this transaction.
+     * @param payload a application defined data payload in byte array format.
+     * @param dependencies a list of transactionIds that this transaction depends on.
      * @return true, if the transaction is valid.
      */
-    public boolean addTransaction(VegvisirApplicationContext context, Block.Transaction tx);
+    public boolean addTransaction(VegvisirApplicationContext context,
+                                  String topic,
+                                  byte[] payload,
+                                  List<Block.Transaction.TransactionId> dependencies);
 }
